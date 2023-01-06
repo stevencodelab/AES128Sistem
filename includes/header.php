f6687f6690ca878eef11d154f1bef6d2e75e0e20
Steven Morison <46344837+AntiTeror88@users.noreply.github.com>
2023-01-07 01:35:00 +0700

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -12,12 +12,13 @@
     <link rel="icon" type="image/x-icon" href="img/icon.png">
 
     <!-- Custom fonts for this template-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"rel="stylesheet">
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
     <link href="css/jquery.dataTables.min.css" rel="stylesheet">
-
+    <link href="css/dataTables.bootstrap5.min.css" rel="stylesheet">
 </head>
 
 <body id="page-top">
