bd3ac8ae1a7dbe09a109e1576c47a2b9dedb481c
Steven Morison <46344837+AntiTeror88@users.noreply.github.com>
2023-01-07 01:35:50 +0700

    Update scripts.php

--- includes/scripts.php
+++ includes/scripts.php
@@ -2,6 +2,7 @@
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="js/jquery-3.6.3.min.js"></script>
+    
 
     <!-- Core plugin JavaScript-->
     <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
@@ -13,9 +14,37 @@
     <script src="vendor/chart.js/Chart.min.js"></script>
 
     <!-- Page level custom scripts -->
-    <script src="js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+    
+    <script src="js/jquery.dataTables.min.js"></script>
     <script>
         $(document).ready( function () {
     $('#tabelku').DataTable();
 } );
     </script>
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"  crossorigin="anonymous"></script>
+<script>
+        $(document).ready(function () {
+
+            $('.deletebtn').on('click', function () {
+
+                $('#deletemodal').modal('show');
+
+                $tr = $(this).closest('tr');
+
+                var data = $tr.children("td").map(function () {
+                    return $(this).text();
+                }).get();
+
+                console.log(data);
+
+                $('#delete_id').val(data[0]);
+
+            });
+        });
+    </script>
+    
+
+    
