72123f69daf08beed1bdff99ce338cedf0a77313
Steven Morison <46344837+AntiTeror88@users.noreply.github.com>
2023-01-09 23:03:30 +0700

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,56 @@
+<?php 
+$title = 'AES 128 Homepage';
+
+include('konten/header.php');
+include('konten/navbar.php');
+?>
+
+<div class="py-5">
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+        <div class="container-fluid px-4">
+<h1 class="mt-4">Enkripsi Algoritma Advanced Encryption Standart (AES) 128</h1>
+<ul class="breadcrumb mb-4">
+<li><i class="">&nbsp;</i></li>
+              <a>Tentang AES 128</a>
+              
+            </ul>
+              
+              
+            </ul>
+
+
+<div class="row">
+<div class="col-md-12">
+  <div class="card">
+  <div class="card-header">
+    <h4></h4>
+
+  </div>
+  <div class="card-body">
+  <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">Advanced Encryption Standart (AES) Algorithm</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+  <form class="form-horizontal" method="" action="" enctype="multipart/form-data">
+    
+  <fieldset>
+    
+                        <legend class="justify">Advanced Encryption Standard (AES) merupakan algoritma cryptographic yang dapat digunakan untuk
+mengamakan data. Algoritma AES adalah blok chipertext simetrik yang dapat mengenkripsi (encipher)
+dan dekripsi (decipher) informasi. Proses Enkripsi adalah proses merubah data dari bentuk asli kedalam bentuk ciphertext;
+sebaliknya dekripsi adalah merubah ciphertext data menjadi bentuk semula
+sebagai plaintext. Algoritma AES  mengunakan kunci kriptografi 128, 192, dan 256 bits untuk mengenkrip
+dan dekrip data pada blok 128 bits</legend>
+        </div>
+    </div>
+</div>
+
+</div>
+        </div></div></div></div></div>
+<?php 
+include('includes/footer.php');
+?>
